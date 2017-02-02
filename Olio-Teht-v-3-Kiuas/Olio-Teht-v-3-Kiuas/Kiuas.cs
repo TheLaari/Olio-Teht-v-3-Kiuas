1ff6291a8edbe4d0d3a16c586f9218fe2ef1d188
Joni <Joni@LENNU>
2017-02-02 07:43:43 +0200

    Initial commit.

---
+++ Olio-Teht-v-3-Kiuas/Olio-Teht-v-3-Kiuas/Kiuas.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_3_Kiuas
+{
+    class Kiuas
+    {
+        //ominaisuudet
+        public string malli { get; set; }
+        public bool virta { get; set; }
+        public int teho { get; set; }
+        public double lämpötila { get; set; }
+        public double kosteus { get; set; }
+
+        //ei erityistä konstruktoria joten käytetään oletusta.
+
+        //tästä napista se menee päälle
+        public void Virratpäälle()
+        {
+            virta = true;
+        }
+        public void Virratveks()
+        {
+            virta = false;
+        }
+        
+        //tällä säädetään lämpötilaa
+        public void Lisäälämpöä()
+        {
+            lämpötila += 20;
+        }
+        public void Vähemmänlämpöä()
+        {
+            lämpötila -= 20;
+        }
+
+        //tällä säädellään kosteutta
+        public void Lisääkosteutta()
+        {
+            kosteus += 5;
+        }
+        public void Vähemmänkosteutta()
+        {
+            kosteus -= 5;
+        }
+
+        //metodi jolla näytetään kiukaan tiedot
+        public void PrintData()
+        {
+            Console.WriteLine("Kiukaan tiedot : ");
+            Console.WriteLine(" - malli : " + malli);
+            Console.WriteLine(" - virta : " + virta);
+            Console.WriteLine(" - teho : " + teho);
+            Console.WriteLine(" - lämpötila : " + lämpötila);
+            Console.WriteLine(" - kosteus : " + kosteus);
+        }
+
+        //destructor
+        ~Kiuas()
+        {
+            Console.WriteLine("Kiuas-objekti tuhottu.");
+        }
+    }
+}
---
+++ Olio-Teht-v-3-Kiuas/Olio-Teht-v-3-Kiuas/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_Teht_v_3_Kiuas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //luodaan yksi kiuas-instanssi
+            Kiuas helo = new Kiuas();
+            helo.malli = "Ringo Black 60 STJ";
+            helo.virta = true;
+            helo.teho = 6000;
+            helo.lämpötila = 0;
+            helo.kosteus = 45;
+
+            //näytetään helon tiedot
+            helo.PrintData();
+            Console.ReadLine();
+            //Käännä lisää lämpöä
+            helo.Lisäälämpöä();
+            //näytä tiedot uudelleen, huomaa lämpötilan nousu (!)
+            helo.PrintData();
+            Console.ReadLine();
+
+        }
+    }
+}
